2990f56d9ab1f61cc0018e5c0c5190368354f862
ihccenen <110120407+ihccenen@users.noreply.github.com>
2023-03-09 01:24:58 -0300

    Changed to use state and added restart

--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,69 @@
-import { useEffect, useReducer } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Image from './components/Image';
 import Username from './components/Username';
-import { UserAction } from './components/types/UserAction.types';
 import validateUserPlay from './firebase/validateUserPlay';
 
 function msToTime(milliseconds: number) {
   return new Date(milliseconds).toISOString().slice(11, 19);
 }
 
-type UserInfo = {
-  username: string | null;
-  gameStart: number;
-  message: string | null;
-  foundCharacters: string[];
-  gameEnd: number;
-  scores: number[];
-};
-
-const initialState = {
-  username: null,
-  gameStart: 0,
-  message: null,
-  foundCharacters: [],
-  gameEnd: 0,
-  scores: [],
-};
-
-const reducer = (state: UserInfo, action: UserAction) => {
-  switch (action.type) {
-    case 'setUsername':
-      return { ...state, username: action.payload };
-    case 'setStart':
-      return { ...state, gameStart: Date.now() };
-    case 'setEnd':
-      return { ...state, message: 'All characters have been found', gameEnd: Date.now() };
-    case 'setMessage':
-      return { ...state, message: action.payload };
-    case 'addFoundCharacter':
-      return {
-        ...state,
-        message: 'Correct',
-        foundCharacters: state.foundCharacters.concat(action.payload),
-      };
-    case 'addScore':
-      return { ...state, scores: state.scores.concat(state.gameEnd - state.gameStart) };
-    case 'restart':
-      return {
-        ...state,
-        gameStart: Date.now(),
-        foundCharacters: [],
-      };
-    default:
-      return state;
-  }
-};
-
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [username, setUsername] = useState<string | null>(null);
+  const [foundPeople, setFoundPeople] = useState<string[]>([]);
+  const [msg, setMsg] = useState<string | null>(null);
+  const [timer, setTimer] = useState(0);
+  const [userScores, setUserScores] = useState<number[]>([]);
 
-  const validatePlay = (data: { x: number; y: number; character: string }) => {
-    const { character } = data;
+  const isGameOver = foundPeople.length > 4;
+  if (isGameOver) {
+    setUserScores((prev) => prev.concat(Date.now() - timer));
+    setFoundPeople([]);
+  }
 
-    dispatch({ type: 'setMessage', payload: 'Validating' });
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>, name: string) => {
+    e.preventDefault();
 
-    validateUserPlay(data)
-      .then(() => dispatch({ type: 'addFoundCharacter', payload: character }))
-      .catch(() => dispatch({ type: 'setMessage', payload: 'Wrong character or coordinates' }));
+    setTimer(Date.now());
+    setUsername(name);
   };
 
-  useEffect(() => {
-    if (state.foundCharacters.length === 5) {
-      dispatch({ type: 'setEnd' });
-      dispatch({ type: 'addScore' });
-      dispatch({ type: 'restart' });
-    }
-  }, [state.foundCharacters]);
+  const validatePlay = (data: { name: string; x: number; y: number }) => {
+    const { name } = data;
+
+    setMsg('Validating');
+    validateUserPlay(data)
+      .then(() => {
+        setFoundPeople((prev) => prev.concat(name));
+        setMsg('Correct');
+      })
+      .catch(() => setMsg('Wrong coordinates or person'));
+  };
+
+  const handleRestartClick = () => {
+    setTimer(Date.now());
+    setFoundPeople([]);
+    setMsg('Game has been restarted');
+  };
 
   return (
     <div>
-      {state.username ? (
-        <Image foundCharacters={state.foundCharacters} validatePlay={validatePlay} />
+      {username ? (
+        <>
+          <Image foundPeople={foundPeople} validatePlay={validatePlay} />
+          <button type="button" onClick={handleRestartClick}>
+            Restart
+          </button>
+        </>
       ) : (
-        <Username dispatch={dispatch} />
+        <Username handleSubmit={handleSubmit} />
       )}
-      {state.message && <p>{state.message}</p>}
-      {state.scores[0] && (
+      {msg && <p>{msg}</p>}
+      {userScores[0] && (
         <div>
           Scores:
-          {state.scores.map((s) => (
-            <p key={Math.random()}>{msToTime(s)}</p>
+          {userScores.map((ms) => (
+            <p key={Math.random()}>{msToTime(ms)}</p>
           ))}
         </div>
       )}
