8e7763cdf62eb2705f269c7595c0197709a0199d
ihccenen <110120407+ihccenen@users.noreply.github.com>
2023-03-09 01:16:40 -0300

    Changed to form and removed dispatch

--- src/components/Username.tsx
+++ src/components/Username.tsx
@@ -1,25 +1,21 @@
 import { useState } from 'react';
-import { UserAction } from './types/UserAction.types';
 
-export default function Username({ dispatch }: { dispatch: React.Dispatch<UserAction> }) {
+export default function Username({
+  handleSubmit,
+}: {
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>, name: string) => void;
+}) {
   const [value, setValue] = useState('');
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => setValue(e.currentTarget.value);
 
-  const handleClick = () => {
-    dispatch({ type: 'setUsername', payload: value });
-    dispatch({ type: 'setStart' });
-  };
-
   return (
-    <div>
+    <form onSubmit={(e) => handleSubmit(e, value)}>
       <label htmlFor="username">
         Username
         <input type="text" name="username" id="username" onChange={handleChange} value={value} />
       </label>
-      <button type="button" onClick={handleClick}>
-        Submit
-      </button>
-    </div>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
