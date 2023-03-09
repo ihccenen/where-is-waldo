e8bc9b0c1af78e174d4375478896f3df97f572a1
ihccenen <110120407+ihccenen@users.noreply.github.com>
2023-03-09 01:25:43 -0300

    Changed to name and removed return

--- src/firebase/validateUserPlay.ts
+++ src/firebase/validateUserPlay.ts
@@ -1,8 +1,6 @@
 import { collection, addDoc } from 'firebase/firestore';
 import db from './config';
 
-export default async function validateUserPlay(data: { character: string; x: number; y: number }) {
-  const docRef = await addDoc(collection(db, 'coordinates'), data);
-
-  return Promise.resolve(docRef);
+export default async function validateUserPlay(data: { name: string; x: number; y: number }) {
+  await addDoc(collection(db, 'coordinates'), data);
 }
