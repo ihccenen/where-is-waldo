771975fd9603241316dc736405e58c644d9ddea1
ihccenen <110120407+ihccenen@users.noreply.github.com>
2023-03-12 15:26:35 -0300

    Added add score and scoreboards

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import Image from './components/Image';
-import Username from './components/Username';
+import addScore from './firebase/addScore';
+import getLeaderboard from './firebase/getLeaderboard';
+import updateData from './firebase/updateUsername';
 import validateUserPlay from './firebase/validateUserPlay';
-
-function msToTime(milliseconds: number) {
-  return new Date(milliseconds).toISOString().slice(11, 19);
-}
+import UsersScore from './types/UserScore.type';
+import UserSubmit from './types/UserSubmit.type';
+import Username from './components/Username';
+import Image from './components/Image';
+import Scoreboard from './components/Scoreboard';
 
 export default function App() {
-  const [username, setUsername] = useState<string | null>(null);
+  const [leaderboard, setLeaderboard] = useState<UsersScore[]>([]);
+  const [username, setUsername] = useState<string>('');
   const [foundPeople, setFoundPeople] = useState<string[]>([]);
   const [msg, setMsg] = useState<string | null>(null);
   const [timer, setTimer] = useState(0);
-  const [userScores, setUserScores] = useState<number[]>([]);
+  const [userScores, setUserScores] = useState<UsersScore[]>([]);
+  const [showForm, setShowForm] = useState(false);
+  const [highScoreId, setHighScoreId] = useState<string>('');
 
-  const isGameOver = foundPeople.length > 4;
+  const isGameOver = foundPeople.length === 5;
+
   if (isGameOver) {
-    setUserScores((prev) => prev.concat(Date.now() - timer));
     setFoundPeople([]);
+    setUserScores((prev) => prev.concat({ time: Date.now() - timer }));
+    setShowForm(true);
   }
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>, name: string) => {
+  const handleUsernameSubmit: UserSubmit = (e, name) => {
     e.preventDefault();
 
     setTimer(Date.now());
@@ -46,6 +53,29 @@ export default function App() {
     setMsg('Game has been restarted');
   };
 
+  const handleScoreSubmit: UserSubmit = (e, name) => {
+    e.preventDefault();
+
+    const { time } = userScores[userScores.length - 1];
+
+    addScore({ username: name, time })
+      .then(setHighScoreId)
+      .catch((err) => setMsg(err.message));
+
+    setShowForm(false);
+  };
+
+  const handleUpdateDataSubmit: UserSubmit = (e, name) => {
+    e.preventDefault();
+
+    updateData(name, highScoreId);
+    setHighScoreId('');
+  };
+
+  useEffect(() => {
+    getLeaderboard().then((res) => setLeaderboard(res.leaderboard));
+  }, []);
+
   return (
     <div>
       {username ? (
@@ -56,17 +86,25 @@ export default function App() {
           </button>
         </>
       ) : (
-        <Username handleSubmit={handleSubmit} />
+        <Username handleSubmit={handleUsernameSubmit} />
       )}
       {msg && <p>{msg}</p>}
       {userScores[0] && (
-        <div>
-          Scores:
-          {userScores.map((ms) => (
-            <p key={Math.random()}>{msToTime(ms)}</p>
-          ))}
-        </div>
+      <div>
+        Scores:
+        {' '}
+        <Scoreboard scores={userScores} />
+      </div>
       )}
+      {leaderboard[0] && (
+      <div>
+        Leaderboard:
+        {' '}
+        <Scoreboard scores={leaderboard} />
+      </div>
+      )}
+      {showForm && <Username handleSubmit={handleScoreSubmit} />}
+      {highScoreId && <Username handleSubmit={handleUpdateDataSubmit} />}
     </div>
   );
 }
